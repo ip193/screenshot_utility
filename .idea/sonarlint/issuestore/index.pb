9550feabcdf09a822ad89193e940eff39f0e39c6
Jakob Weichselbaumer <j.weichse@gmail.com>
2020-12-27 00:34:32 +0100

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+=
+manifest.json,c\a\ca642a22686971b261b60de81d8ff554fdd91e2e
+8
+popup.js,0\e\0eb7f254729257427b56a71afa8b1f15ab0d004d
+:
+
+popup.html,6\a\6a292f9ab3d658b2908a708077dc82f45eef57d9
+9
+	infos.txt,c\a\ca427f405561a8a6aa7b4812205472e4da6334d0
+=
+background.js,6\e\6e26760b966a94d338e350c9cf0bc99b907785d6
+=
+js/epicker.js,0\6\062f07bd434b5e826c14e13f795e1bdbdf8e19ee
+A
+js/vapi-client.js,c\6\c6eedac827bb7915d1f7a772e79953ad841de8dd
+:
+
+js/vapi.js,2\8\28fc4e6ecc0970f1ab76a7a3871a2c5c8c447d9f
---
+++ popup.js
@@ -0,0 +1,28 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+let changeColor = document.getElementById('changeColor');
+let snipButton = document.getElementById("snip-button")
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+  console.log("popup.js: changeColor::onclick");
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
+
+snipButton.onclick = function(element){
+  console.log("popup.js: starting element picker");
+  window.element_picker_exec();
+}
